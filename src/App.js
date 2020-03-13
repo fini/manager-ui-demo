07ad08b9b47c0f40f35e5460a6bd5345ea0983af
Fini Alring <fini.alring@gmail.com>
2020-03-13 20:54:54 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FilteredInputField from './components/FilteredInputField';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header">        
+        <h3>Filtered Input Selection Demo</h3>
+        <h4>by Fini Alring, &copy;2020</h4>
       </header>
+
+      <FilteredInputField 
+          title="Favorite Manager"
+          name="test"
+          placeholder="Choose Manager..."
+          dataUrl="https://gist.githubusercontent.com/daviferreira/41238222ac31fe36348544ee1d4a9a5e/raw/5dc996407f6c9a6630bfcec56eee22d4bc54b518/employees.json"
+        />
+        <br />
+        <br />
+        <button>Continue</button>
     </div>
   );
 }
